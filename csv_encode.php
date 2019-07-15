94bd64c42bc57eecfd4ede47e29c22e697bbcb9c
Amberliu0229 <a99133135@gmail.com>
2019-07-15 19:07:32 +0800

    Add files via upload
    need to add explode => json_encode => csv

--- csv_encode.php
+++ csv_encode.php
@@ -1,35 +1,46 @@
 <?php 
-header('Content-Encoding: UTF-8');
-header('Content-type: text/csv; charset=UTF-8');
 
 $fpread = fopen('test.csv', 'r');
-$fpwrite = fopen('write.txt', 'w');
+$fpwrite = fopen('write.csv', 'w');
 $max_line = 2000;
 $delimiter = ",";
+
 if ($fpread !== FALSE){
     while ($data = fgetcsv($fpread,$max_line,$delimiter)) {
-    
         $num = count($data);
         $row++;  
         if($row != 1 ){
             for ($c=0; $c < $num; $c++) {
-                fwrite($fpwrite,$data[$c]);
+                // fwrite($fpwrite,$data[$c]);
+                // fwrite($fpwrite,"\n");
+                $s = serialize($data[$c]);
+                // print($s);
+                $str = str_replace(array('[',']'),array('',''),$s);
+                $newstr = explode(",",$str);
+                // printf($str);
+                // print("\n");
+                $unserialized_data = unserialize($newstr);
+                fwrite($fpwrite,$newstr);
                 fwrite($fpwrite,"\n");
             }
-            $json = json_encode($data);
-            array_walk_recursive($data, function(&$value, $key) {
-                if(is_string($value)) {
-                    $value = urlencode($value);
-                }
-            });
-            $json = urldecode(json_encode($data));
-            print_r($json);
-            print_r("\n");
+            // print_r($data);
+            // $json = json_encode($data,JSON_UNESCAPED_UNICODE);
+            // print_r($json);
+            // print_r("\n");
+
+            $data_json_url = json_decode($json,true);
         }
     }
+}else{
+    printf("CSV D.N.E");
 }
 
-
+function _removeBOM($str = '') {
+    if (substr($str, 0, 3) == pack("CCC", 0xef, 0xbb, 0xbf)) {
+        $str = substr($str, 3);
+    }
+    return $str;
+}
 fclose($fpread);
 fclose($fpwrite);
 ?>
