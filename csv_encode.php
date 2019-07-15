7fee0b1925f874c640c979558bb1fe6caf0e9c7d
Amberliu0229 <a99133135@gmail.com>
2019-07-15 23:35:54 +0800

    Add files via upload

--- csv_encode.php
+++ csv_encode.php
@@ -1,10 +1,8 @@
 <?php 
-
 $fpread = fopen('test.csv', 'r');
 $fpwrite = fopen('write.csv', 'w');
 $max_line = 2000;
 $delimiter = ",";
-
 if ($fpread !== FALSE){
     while ($data = fgetcsv($fpread,$max_line,$delimiter)) {
         $num = count($data);
@@ -17,24 +15,23 @@ if ($fpread !== FALSE){
                 // print($s);
                 $str = str_replace(array('[',']'),array('',''),$s);
                 $newstr = explode(",",$str);
-                // printf($str);
+                $json = json_encode($newstr);
+                printf($json); 
                 // print("\n");
-                $unserialized_data = unserialize($newstr);
-                fwrite($fpwrite,$newstr);
+                // $unserialized_data = unserialize($newstr);
+                fwrite($fpwrite,$json);
                 fwrite($fpwrite,"\n");
             }
             // print_r($data);
             // $json = json_encode($data,JSON_UNESCAPED_UNICODE);
             // print_r($json);
             // print_r("\n");
-
             $data_json_url = json_decode($json,true);
         }
     }
 }else{
     printf("CSV D.N.E");
 }
-
 function _removeBOM($str = '') {
     if (substr($str, 0, 3) == pack("CCC", 0xef, 0xbb, 0xbf)) {
         $str = substr($str, 3);
